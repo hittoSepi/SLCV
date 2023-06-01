1bd21435a7c429abeddf2f1049f8f7a09bc827f0
Seppo Laine <hittoaeae@gmail.com>
2023-06-01 22:22:53 +0300

    committia

--- src/components/GitHub.vue
+++ src/components/GitHub.vue
@@ -5,15 +5,17 @@
                 Source code
             </h2>
             <div class="row">
-                <div class="col-sm-12 col-md-4">
-                    <ul class="list-unstyled">
-                        <li v-for="file in repo.files" v-if="isSourceFile(file)"><span v-on:click="linkClick"
-                                class="source-link" v-bind:data-url="file.url">{{ file.path }}</span></li>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <strong>Files</strong>
+                    <ul class="file__listing list-unstyled list-group">
+                        <li :class="file.selected == true ? 'active source-link list-group-item' : 'source-link list-group-item'" v-on:click="linkClick" class="source-link list-group-item" v-bind:data-url="file.url" v-bind:data-path="file.path" v-for="file in repo.files" v-if="isSourceFile(file)">
+                            <span >{{ parseFileNameFromPath(file) }}</span>
+                        </li>
                     </ul>
                 </div>
-                <div class="col-sm-12 col-md-8">
+                <div class="col-sm-12 col-md-8 col-lg-8 ">
                     <div v-if="selectedFile">
-                        <p>{{ selectedFile.path }}</p>
+                        <strong>{{ parseFileNameFromPath(selectedFile) }}</strong>
                         <highlight-code auto >{{ selectedFileContents }}</highlight-code>
                     </div>
                 </div>
@@ -30,7 +32,7 @@ export default {
     data() {
         return {
             repo: {
-                readOnlyToken: "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",
+                readOnlyToken: "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",
                 name: "SLCV",
                 apiUrl: "https://api.github.com/repos/hittoSepi/SLCV/git/trees/main?recursive=1",
                 files: [],
@@ -44,28 +46,34 @@ export default {
         var _self = this;
 
         this.headers = new Headers();
-  //      this.headers.append("Authorization", "Bearer " + this.repo.readOnlyToken);
+        this.headers.append("Authorization", "Bearer " + this.repo.readOnlyToken);
         this.headers.append("X-GitHub-Api-Version", "2022-11-28")
 
 
         fetch(this.repo.apiUrl, { headers: _self.headers })
             .then(response => response.json())
             .then(data => {
-                this.repo.files = data.tree.reverse();
-                // check if app.js exists and select it
+               
+                data.tree.forEach(file => {
+                    _self.repo.files.push({url:file.url, path:file.path, type:file.type, selected:false});
+                });
+
                 let appJs = this.repo.files.find(file => file.path == "src/main.js");
                 if (appJs) {
+                    appJs.selected = true;
                     _self.selectFile(appJs);
                 }
             });
     },
     methods: {
-        async selectFile(file) {
+        async selectFile(f) {
+            let _file = this.repo.files.find(file => file.path == f.path);
             var _self = this;
-            this.selectedFile = file;
+            _file.selected = true;
+            this.selectedFile = _file;
             this.selectedFileContents = null;
 
-            await fetch(file.url, { headers: _self.headers })
+            await fetch(_file.url, { headers: _self.headers })
                 .then(response => response.json())
                 .then(data => {
                     _self.selectedFileContents = atob(data.content);
@@ -74,14 +82,23 @@ export default {
 
 
         },
+      
         linkClick(event) {
             event.preventDefault();
-            this.selectFile({ url: event.target.dataset.url, path: event.target.innerText });
+            this.repo.files.forEach(file => {
+                file.selected = false;
+            });
+            let _file = this.repo.files.find(file => file.path == event.target.dataset.path);
+            this.selectFile(_file);
         },
       
         isSourceFile(file) {
-            return file.type == "blob" && file.path.match(/\.(vue|js|html|css)$/i) && !file.path.match(/\.(min.css)$/i) && !file.path.match(/(build)/i) && !file.path.match(/(build|config)/i) && !file.path.match(/^\./i);
+            return file.type == "blob" && file.path.match(/\.(vue|js|html|css|json|less)$/i) && !file.path.match(/(.min.css|lock.json)$/i) && !file.path.match(/(build)/i) && !file.path.match(/(build|config)/i) && !file.path.match(/^\./i);
         },
+        parseFileNameFromPath(file) {
+            return file.path.split("/").pop();
+        },
+
     }
 }
 </script>
